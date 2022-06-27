7b12b6aa11306d9c5759bb7a78259950e7bb1265
abhishekicha <apramanik28@gmail.com>
2022-06-27 15:23:23 +0530

    15 full section

--- cypress/integration/2-advanced-examples/ApiTest.js
+++ cypress/integration/2-advanced-examples/ApiTest.js
@@ -17,11 +17,14 @@ describe('My First Test Suite', function(){
                 {"book_name":"RestAssured with Java",
                 "isbn":"RSU",
                 "aisle":"2301"},
+               
             ]
         }).as('bookretrievals')
 
         cy.get("button[class='btn btn-primary']").click()
-        cy.wait('@bookretrievals')
+        cy.wait('@bookretrievals').should(({request, response})=>{
+            cy.get('tr').should('have.length', response.body.length+1)
+        })
         cy.get('p').should('have.text', 'Oops only 1 Book available')
 
     })
